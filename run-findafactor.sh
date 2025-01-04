537da558217ac80b296db21c1a74c6289578bc63
Aryan Blaauw <ablaauw@gmx.net>
2025-01-04 11:11:03 +0000

    Update run-findafactor.sh

--- run-findafactor.sh
+++ run-findafactor.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-#
 
-for i in {65535..4294967295..65535}
-    do
-	prime=$(matho-primes -c 1 -u $i)
-	echo $(($prime + $prime)) > i2
-	i2=$(<i2)
-	prime2=$(matho-primes -c 1 -u $i2)
-	echo $(($prime * $prime2)) > fact
-	fact=$(<fact)
-	python3 findafactor.py $fact
+for i in {65535..4294967295..65535}; do
+  prime=$(matho-primes -c 1 -u $i)
+  i2=$((prime + prime))
+  prime2=$(matho-primes -c 1 -u $i2)
+  fact=$((prime * prime2))
+  python3 findafactor.py $fact
 done

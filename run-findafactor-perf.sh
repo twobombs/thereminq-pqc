9a1e4c131e912f88ff1b88e9cc502cebe5974cc0
Aryan Blaauw <ablaauw@gmx.net>
2025-01-09 15:53:39 +0000

    Create run-findafactor-perf.sh

---
+++ run-findafactor-perf.sh
@@ -0,0 +1,11 @@
+# benchmarking numbers across variables
+# wheel and gear
+
+echo 13426308670647251507 | python3 run-findafactor-perf.py $1
+echo 65396930444821993081 | python3 run-findafactor-perf.py $1
+echo 235692174099029395493 | python3 run-findafactor-perf.py $1
+echo 1131139246084645680307 | python3 run-findafactor-perf.py $1
+echo 4609584385352723128631 | python3 run-findafactor-perf.py $1
+echo 12219341628304967791861 | python3 run-findafactor-perf.py $1
+echo 56784026475815417443757 | python3 run-findafactor-perf.py $1
+echo 218386293684309373970141 | python3 run-findafactor-perf.py $1
